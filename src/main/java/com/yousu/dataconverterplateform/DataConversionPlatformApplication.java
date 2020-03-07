473742d823a1d2887a748fda2d27aa4b5eba9b29
peiqingliu <1226880979@qq.com>
2020-03-07 10:14:51 +0800

    数据转换平台

---
+++ src/main/java/com/yousu/dataconverterplateform/DataConversionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yousu.dataconverterplateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConversionPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataConversionPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yousu/dataconverterplateform/DataConversionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yousu.dataconverterplateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConversionPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
